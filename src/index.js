6f8137d59798caf31d2d11f91c845cf75245fbc3
Caio Messias <caio.messias@gerencianet.com.br>
2021-11-26 01:55:39 +0000

    Implementa rota para atualizar nome e retorna conta

--- src/index.js
+++ src/index.js
@@ -106,6 +106,21 @@ app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
   return response.json(statement);
 });
 
+app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
 app.listen(PORT, () => {
   console.log(`Server listen in port ${PORT}`);
 });
